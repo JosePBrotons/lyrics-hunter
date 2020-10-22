fc38335f9cc716df119ad32a891974fc06381879
Jose P. Brotons R <jose.brotons@almundo.com>
2020-10-21 21:22:43 -0500

    Added tabBar icons into the router.

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,18 +1,49 @@
 import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
+import { NavigationContainer, RouteProp } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import FontAwesome from 'react-native-vector-icons/FontAwesome'
 import { tabs } from './routes';
-import { ITab } from './interface';
+import { IScreenOptions, ITab, ITabIconProps } from './interface';
+import { COLORS } from '../constants';
 
 const Tab = createBottomTabNavigator();
+
+const tabBarOptions = {
+    activeTintColor: COLORS.blue,
+    inactiveTintColor: COLORS.gray
+}
 
 const renderTabs = tabs.map((tab: ITab, index: number) => (
     <Tab.Screen key={`tab-${index}`} {...tab} />
 ));
 
+const getTabIconByRouteName = (routeName: string) => {
+    const tabIconByRouteName = {
+        'Search': 'search',
+        'History': 'history',
+        'DEFAULT': 'info-circle'
+    }
+    const route: string = !!routeName ? routeName : 'DEFAULT'
+    return tabIconByRouteName[route as keyof typeof tabIconByRouteName]
+}
+
+const renderTabIcon = (route: RouteProp<Record<string, object | undefined>, string>) => {
+    return (props: ITabIconProps) => {
+        const { size = 10, color = 'tomato' } = { ...props }
+        const { name = '' } = { ...route }
+        const iconName = getTabIconByRouteName(name);
+        return <FontAwesome name={iconName} size={size} color={color} />;
+    }
+}
+
+const getScreenOptions = (screenOptions: IScreenOptions) => {
+    const { route } = { ...screenOptions }
+    return { tabBarIcon: renderTabIcon(route) }
+}
+
 const Router = () => {
     return (<NavigationContainer>
-        <Tab.Navigator>
+        <Tab.Navigator screenOptions={getScreenOptions} tabBarOptions={tabBarOptions}>
             {renderTabs}
         </Tab.Navigator>
     </NavigationContainer>);
--- src/router/interface.ts
+++ src/router/interface.ts
@@ -1,3 +1,5 @@
+import { RouteProp } from '@react-navigation/native';
+
 export interface IOptions {
     title: string;
 }
@@ -6,6 +8,17 @@ export interface IStack {
     name: string;
     component: (props: any) => JSX.Element;
     options?: IOptions;
+}
+
+export interface ITabIconProps {
+    focused: boolean;
+    color: string;
+    size: number;
+}
+
+export interface IScreenOptions {
+    route: RouteProp<Record<string, object | undefined>, string>;
+    navigation: any;
 }
 
 export interface ITab {
